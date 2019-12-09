46108783fc147dbef401736bd5f8ec133fb1e872
Ryan Rampersad <ryan.rampersad@gmail.com>
2019-12-09 09:00:38 -0600

    Adds additional hooks for children positions

--- src/layout/GlobalLayout.tsx
+++ src/layout/GlobalLayout.tsx
@@ -5,11 +5,17 @@ import { Theme } from '../theme';
 
 export interface GlobalLayoutProps {
   theme: Theme;
+  beforeGlobalStyles?: React.ReactNode;
+  afterGlobalStyles?: React.ReactNode;
+  afterChildren?: React.ReactNode;
 }
 
-export const GlobalLayout = ({theme, children}) => {
+export const GlobalLayout: React.FC<GlobalLayoutProps> = ({theme, beforeGlobalStyles = null, afterGlobalStyles = null, afterChildren = null, children}) => {
   return <ThemeProvider theme={theme}>
+    {beforeGlobalStyles}
     <GlobalStyles />
+    {afterGlobalStyles}
     {children}
+    {afterChildren}
   </ThemeProvider>
 };
